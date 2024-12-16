13360152ecd5f96d7810249711df6ffb5b70a1d1
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-12-16 14:41:11 +0100

    chore: nightly job (#52)
    * chore: nightly job
    
    * tweak
    
    * tweak
    
    * fix
    
    * fix
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * clean up
    
    * add ios
    
    * remove ios
    
    * cleanup
    
    * .

--- scripts/verdaccio-reset.sh
+++ scripts/verdaccio-reset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 npm config set registry https://registry.npmjs.org/
-echo "link-workspace-packages=true\nauto-install-peers=true" > ~/.npmrc
+echo "link-workspace-packages=true\nauto-install-peers=true" > .npmrc
 rm -rf /tmp/verdaccio-storage
