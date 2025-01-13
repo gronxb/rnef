8e8260bac59a73e821b9d15cdfd7f7da57f50eb8
Michał Pierzchała <thymikee@gmail.com>
2025-01-13 14:04:07 +0100

    fix(ios): picking non-project locations (#92)
    * fix(ios): picking non-project locations
    
    * better verbose handling; remove logging stdout from xcodebuild
    
    * remove unused import
    
    * update test and filter logic
    
    * add logging in verbose mode too

--- packages/plugin-platform-apple/src/lib/commands/build/buildProject.ts
+++ packages/plugin-platform-apple/src/lib/commands/build/buildProject.ts
@@ -83,11 +83,20 @@ export const buildProject = async (
   }
 
   const loader = spinner();
-  loader.start(
-    `${
-      args.archive ? 'Archiving' : 'Building'
-    } the app with xcodebuild for ${scheme} scheme in ${mode} mode.`
-  );
+  if (!logger.isVerbose()) {
+    loader.start(
+      `${
+        args.archive ? 'Archiving' : 'Building'
+      } the app with xcodebuild for ${scheme} scheme in ${mode} mode.`
+    );
+  } else {
+    // @todo abstract loader to fall back to logger in non-tty mode.
+    logger.info(
+      `${
+        args.archive ? 'Archiving' : 'Building'
+      } the app with xcodebuild for ${scheme} scheme in ${mode} mode.`
+    );
+  }
   logger.debug(`Running "xcodebuild ${xcodebuildArgs.join(' ')}.`);
   try {
     const childProcess = spawn('xcodebuild', xcodebuildArgs, {
@@ -96,19 +105,29 @@ export const buildProject = async (
     });
     setupChildProcessCleanup(childProcess);
     const { output } = await childProcess;
-    loader.stop(
-      `${
-        args.archive ? 'Archived' : 'Built'
-      } the app with xcodebuild for ${scheme} scheme in ${mode} mode.`
-    );
+    if (!logger.isVerbose()) {
+      loader.stop(
+        `${
+          args.archive ? 'Archived' : 'Built'
+        } the app with xcodebuild for ${scheme} scheme in ${mode} mode.`
+      );
+    } else {
+      // @todo abstract loader to fall back to logger in non-tty mode.
+      logger.info(
+        `${
+          args.archive ? 'Archived' : 'Built'
+        } the app with xcodebuild for ${scheme} scheme in ${mode} mode.`
+      );
+    }
     return output;
   } catch (error) {
-    logger.log((error as SubprocessError).stdout);
-    logger.error((error as SubprocessError).stderr);
-    loader.stop(
-      'Running xcodebuild failed. Check the error message above for details.',
-      1
-    );
+    if (!logger.isVerbose()) {
+      logger.error((error as SubprocessError).stderr);
+      loader.stop(
+        'Running xcodebuild failed. Check the error message above for details.',
+        1
+      );
+    }
 
     if (!xcodeProject.isWorkspace) {
       throw new RnefError(
--- packages/plugin-platform-apple/src/lib/utils/__tests__/getInfo.test.ts
+++ packages/plugin-platform-apple/src/lib/utils/__tests__/getInfo.test.ts
@@ -15,16 +15,34 @@ vi.mock('fs', () => ({
   readFileSync: vi.fn(),
 }));
 
-describe('getInfo', () => {
-  it('handles non-project / workspace locations in a ', () => {
-    const name = `YourProjectName`;
+const projectsOutput = `{
+  "project" : {
+    "configurations" : [
+      "Debug",
+      "Test",
+      "Release"
+    ],
+    "name" : "YourProjectName",
+    "schemes" : [
+      "YourProjectName-Dev",
+      "YourProjectName-Prod",
+      "NotificationService",
+      "TodayWidget"
+    ],
+    "targets" : [
+      "YourProjectName",
+      "YourProjectNameTests",
+      "TodayWidget",
+      "NotificationService"
+    ]
+  }
+}`;
 
-    vi.mocked(fs.readFileSync)
-      .mockReturnValueOnce(`<?xml version="1.0" encoding="UTF-8"?>
+const workspaceDataFileContents = `<?xml version="1.0" encoding="UTF-8"?>
 <Workspace
    version = "1.0">
    <FileRef
-      location = "group:${name}.xcodeproj">
+      location = "group:YourProjectName.xcodeproj">
    </FileRef>
    <FileRef
       location = "group:Pods/Pods.xcodeproj">
@@ -32,18 +50,52 @@ describe('getInfo', () => {
    <FileRef
       location = "group:container/some_other_file.mm">
    </FileRef>
-</Workspace>`);
+   <FileRef
+      location = "group:storekit/file.storekit">
+   </FileRef>
+</Workspace>`;
 
-    (spawn as Mock).mockResolvedValue({ stdout: '{}' });
+describe('getInfo', () => {
+  it('handles non-project / workspace locations in a ', async () => {
+    vi.mocked(fs.readFileSync).mockReturnValueOnce(workspaceDataFileContents);
 
-    getInfo({ isWorkspace: true, name } as XcodeProjectInfo, 'some/path');
+    (spawn as Mock).mockResolvedValue({ stdout: projectsOutput });
+
+    const info = await getInfo(
+      { isWorkspace: true, name: 'YourProjectName' } as XcodeProjectInfo,
+      'some/path'
+    );
 
     // Should not call on Pods or the other misc groups
     expect(spawn).toHaveBeenCalledWith(
       'xcodebuild',
-      ['-list', '-json', '-project', `some/path/${name}.xcodeproj`],
+      ['-list', '-json', '-project', `some/path/YourProjectName.xcodeproj`],
       { stdio: ['ignore', 'pipe', 'inherit'], cwd: 'some/path' }
     );
+    expect(spawn).toHaveBeenCalledTimes(1);
+
+    expect(info).toMatchInlineSnapshot(`
+      {
+        "configurations": [
+          "Debug",
+          "Test",
+          "Release",
+        ],
+        "name": "YourProjectName",
+        "schemes": [
+          "YourProjectName-Dev",
+          "YourProjectName-Prod",
+          "NotificationService",
+          "TodayWidget",
+        ],
+        "targets": [
+          "YourProjectName",
+          "YourProjectNameTests",
+          "TodayWidget",
+          "NotificationService",
+        ],
+      }
+    `);
   });
 
   afterEach(() => {
