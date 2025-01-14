c08cd3137142b23bb836412725210ce8f7c7726e
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2025-01-14 14:26:39 +0100

    fix: `process.stdin.setRawMode` throwing on CI (#95)

--- packages/tools/src/lib/setupChildProcessCleanup.ts
+++ packages/tools/src/lib/setupChildProcessCleanup.ts
@@ -6,9 +6,12 @@ import type { Subprocess } from 'nano-spawn';
  * This helper is intended for use in long-running tasks like building with xcodebuild, gradlew, bundling, etc.
  */
 export function setupChildProcessCleanup(childProcess: Subprocess) {
-  // overwrite @clack/prompts setting raw mode for spinner and prompts,
-  // which prevents listening for SIGINT and SIGTERM
-  process.stdin.setRawMode(false);
+  // https://stackoverflow.com/questions/53049939/node-daemon-wont-start-with-process-stdin-setrawmodetrue/53050098#53050098
+  if (process.stdin.isTTY) {
+    // overwrite @clack/prompts setting raw mode for spinner and prompts,
+    // which prevents listening for SIGINT and SIGTERM
+    process.stdin.setRawMode(false);
+  }
 
   process.on('SIGINT', () => terminate());
   process.on('SIGTERM', () => terminate());
