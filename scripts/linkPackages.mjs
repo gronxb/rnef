adf5fb9a0047c1f3238897ddc909ee4281ed27d1
Michał Pierzchała <thymikee@gmail.com>
2025-01-17 14:30:39 +0100

    infra: add option to specify package manager to link with (#102)

--- scripts/linkPackages.mjs
+++ scripts/linkPackages.mjs
@@ -3,9 +3,11 @@ import path from 'node:path';
 import glob from 'fast-glob';
 
 const projects = glob.sync('packages/*/package.json');
+const pmArg = process.argv[2] || '--pnpm';
+const pm = pmArg.replace('--', '');
 
 for (const project of projects) {
   const cwd = path.dirname(project);
-  console.log(`Running "pnpm link" in ${cwd}`);
-  await spawn('pnpm', ['link', '--global'], { cwd, stdio: 'inherit' });
+  console.log(`Running "${pm} link" in ${cwd}`);
+  await spawn(pm, ['link', '--global'], { cwd, stdio: 'inherit' });
 }
